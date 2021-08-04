30937aafb80648b04ff1f52b861c933a5b38059b
Nina Brissey <nbrissey@gmail.com>
2021-08-03 18:25:49 -0700

    Add comments for organizational purposes and notes for imports/exports

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,29 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+// import and export syntax video found here
+// https://www.youtube.com/watch?v=_3oSWwapPKQ
+
+// Examples
+// scripts 👇
+// import ActivityRepository from './ActivityRepository';
+// import {
+//   displayAllData,
+//   weeksWaterInput,
+//   displayUserSelectWeek,
+// } from './domUpdates';
+// import getAllData from './apiCalls';
+
+// An example of how you tell webpack to use a CSS (SCSS) file - look at @use here instead of import - must impost ALL scss files here
+// styling 👇
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// An example of how you tell webpack to use an image (also need to link to it in the index.html - example in html)
+// images 👇
+import './images/turing-logo.png';
+
+// An example of exports to the dom file (after all imports put the exports)
+// exports 👇
+// export { currentUser, userRepo, hydrationStats, sleepStats, activityStats };
 
 console.log('This is the JavaScript entry file - your code begins here.');
