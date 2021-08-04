b91ce867aa1f05b10492e49f51e107c2cb85dd86
Nina Brissey <nbrissey@gmail.com>
2021-08-03 18:26:42 -0700

    Add comments for organizational purposes and notes for imports/exports

---
+++ src/domUpdates.js
@@ -0,0 +1,29 @@
+// scripts 👇
+// Examples of what this should look like
+// import { Chart, registerables } from 'chart.js';
+// Chart.register(...registerables);
+// import {
+//   makeWeeklyHydrationChart,
+//   makeDailyHydrationChart,
+//   makeNightsSleepChart,
+//   makeNightsQualityChart,
+//   makeWeeksSleepChart,
+// } from './chartDisplays';
+
+// global variables 👇
+// Example of what importing the global variables from the scripts files looks like
+// import {
+//   currentUser,
+//   userRepo,
+//   hydrationStats,
+//   sleepStats,
+//   activityStats,
+// } from './scripts.js';
+
+// query selectors 👇
+
+// display on load 👇
+
+// display upon user log on 👇
+
+// display upon user input once logged in 👇
