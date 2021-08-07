8c97f85a96a84af569c89a5b321d6dcf75992509
Nina Brissey <nbrissey@gmail.com>
2021-08-07 10:44:19 -0700

    Add basic structure for test, including beforeEach, and will test all properties and start first method build to get pending and current trips

---
+++ test/Traveler-test.js
@@ -0,0 +1,77 @@
+import { expect } from 'chai';
+import Traveler from '../src/Traveler';
+import { travelersData, tripsData } from './test-data';
+import * as dayjs from 'dayjs';
+
+describe('Traveler', () => {
+  let traveler;
+  let trips;
+  let todaysDate;
+
+  beforeEach(() => {
+    trips = tripsData.filter((trip) => trip.userID === 1);
+    traveler = new Traveler(travelersData[0], trips);
+    todaysDate = dayjs().format('YYYY-MM-DD');
+  });
+
+  it('should be a function', () => {
+    expect(Traveler).to.be.a('function');
+  });
+
+  it('should be an instance of Traveler', () => {
+    expect(traveler).to.be.instanceOf(Traveler);
+  });
+
+  it('should have a id', () => {
+    expect(traveler.id).to.equal(1);
+  });
+
+  it('should have a name', () => {
+    expect(traveler.name).to.equal('Ham Leadbeater');
+  });
+
+  it('should have a traveler type', () => {
+    expect(traveler.travelerType).to.equal('relaxer');
+  });
+
+  it('should start with an empty array for all trips', () => {
+    expect(traveler.allTrips).to.be.a('array');
+  });
+
+  it('should start with an empty object for current trip', () => {
+    expect(traveler.currentTrip).to.be.a('object');
+  });
+
+  it('should be able to find a current trip', () => {
+    traveler.getCurrentTrip(todaysDate);
+    expect(traveler.getCurrentTrip).to.be.a('function');
+    expect(traveler.currentTrip.id).to.equal(8);
+  });
+
+  it('should start with an empty array for upcoming trips', () => {
+    expect(traveler.upcomingTrips).to.be.a('array');
+  });
+
+  // it('should be able to get all upcoming trips', () => {
+  //   traveler.getUpcomingTrips();
+  //   expect(traveler.upcomingTrip[0].date);
+  // });
+
+  // it('should start with an empty array for past trips', () => {
+  //   expect(traveler.pastTrips).to.be.a('array');
+  // });
+
+  // it('', () => {});
+
+  // it('should start with an empty array for pending trips', () => {
+  //   expect(traveler.pendingTrips).to.be.a('array');
+  // });
+
+  // it('', () => {});
+
+  // it('', () => {});
+
+  // it('', () => {});
+
+  // it('', () => {});
+});
