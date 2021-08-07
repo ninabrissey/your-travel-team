f5014767a4be4e83128fb0e2f7c169efe1ff04c1
Nina Brissey <nbrissey@gmail.com>
2021-08-07 13:42:01 -0700

    Add all test to complete testing of categorizeTrips function

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -35,7 +35,7 @@ describe('Traveler', () => {
   });
 
   it('should be able to sort all trips by date', () => {
-    traveler.sortAllTrips();
+    traveler.sortAllTripsByDate();
 
     expect(traveler.allTrips[0].date).to.equal('2018/08/28');
   });
@@ -44,34 +44,48 @@ describe('Traveler', () => {
     expect(traveler.pendingTrips).to.deep.equal([]);
   });
 
-  it('', () => {});
+  it('should be able get all pending trips', () => {
+    traveler.sortAllTripsByDate();
+    traveler.categorizeTrips(todaysDate);
+
+    expect(traveler.pendingTrips[0].duration).to.equal(10);
+  });
 
   it('should start with an empty object for current trip', () => {
     expect(traveler.currentTrip).to.be.a('object');
   });
 
-  it('should be able to find a current trip', () => {
-    traveler.getCurrentTrip(todaysDate);
-    expect(traveler.getCurrentTrip).to.be.a('function');
+  it('should be able to get a current trip', () => {
+    traveler.sortAllTripsByDate();
+    traveler.categorizeTrips(todaysDate);
+
     expect(traveler.currentTrip.id).to.equal(8);
   });
 
-  // it('should start with an empty array for upcoming trips', () => {
-  //   expect(traveler.upcomingTrips).to.be.a('array');
+  it('should start with an empty array for upcoming trips', () => {
+    expect(traveler.upcomingTrips).to.deep.equal([]);
+  });
+
+  it('should be able to get all upcoming trips', () => {
+    traveler.sortAllTripsByDate();
+    traveler.categorizeTrips(todaysDate);
+
+    expect(traveler.upcomingTrips[0].date).to.equal('2022/09/16');
+  });
+
+  it('should start with an empty array for past trips', () => {
+    expect(traveler.pastTrips).to.be.a('array');
+  });
+
+  it('should be able to get all past trips', () => {
+    traveler.sortAllTripsByDate();
+    traveler.categorizeTrips(todaysDate);
+    expect(traveler.pastTrips[0].date).to.equal('2019/07/23');
+  });
+
+  // it('should start with the total spent this past year as 0', () => {
+  //   expect(traveler.spendingYTD).to.equal(0);
   // });
-
-  // it('should be able to get all upcoming trips', () => {
-  //   traveler.getUpcomingTrips();
-  //   expect(traveler.upcomingTrip[0].date);
-  // });
-
-  // it('should start with an empty array for past trips', () => {
-  //   expect(traveler.pastTrips).to.be.a('array');
-  // });
-
-  // it('', () => {});
-
-  // it('', () => {});
 
   // it('', () => {});
 
